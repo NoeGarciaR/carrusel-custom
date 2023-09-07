b0a002f941e0d8f565bac861666825dbb2d289b9
Juan Garcia <juan.garcia@epicor.com>
2023-09-07 01:46:19 -0600

    agregamos funcion y paginacion con flechas

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,12 @@
 let productos = document.querySelectorAll('.contenedor-producto');
+let iconosPaginacion = document.querySelectorAll('.paginacion-carrussel img');
+
 const _claseProductoSeleccionado = 'producto-seleccionado';
 const _atributoProductoIndex = 'producto-index';
+const _atributoDireccionPaginacion = 'direccion';
+
 let indexActual = 0;
-let separacionEntreProductos = 20;
+let separacionEntreProductos = 30;
 
 const eliminarClase = (clase) => {
   for (const producto of productos.entries()) {
@@ -20,10 +24,25 @@ const seleccionarProducto = (event) => {
 
 const seleccionarProductoConIndex = (index) => {
   eliminarClase(_claseProductoSeleccionado);
-  productos[index].classList.add(_claseProductoSeleccionado);
+  let _index = index;
+  /** Simulamos un bucle */
+  // intentamos acceder a un producto despues del ultimo
+  // reiniciamos el contador
+  if(index > productos.length - 1) {
+    _index = 0;
+  }
+  // estamos en el primer producto y queremos acceder a un producto antes
+  // seleccionamos el ultimo producto
+  if(index === -1) {
+    _index = productos.length - 1;
+  }
+  // seleccionamos el producto especifico
+  productos[_index].classList.add(_claseProductoSeleccionado);
+  indexActual = _index;
 }
 
 
+// Barrido inicial de las imagenes
 let index = 0;
 for (const producto of productos.entries()) {
   // agregamos liteners
@@ -38,6 +57,26 @@ for (const producto of productos.entries()) {
   producto[1].style.marginRight = `${separacionEntreProductos}px`;
 }
 
+// eliminando el margin right del ultimo elemento
+productos[productos.length -1].style.marginRight = '0px';
+
+
+// Barrido de la paginacion
+iconosPaginacion[0].setAttribute(_atributoDireccionPaginacion, 'izquierda');
+iconosPaginacion[1].setAttribute(_atributoDireccionPaginacion, 'derecha');
+
+for (const icono of iconosPaginacion.entries()) {
+  icono[1].addEventListener("click", (event) => {
+    let direccion = event.target.getAttribute(_atributoDireccionPaginacion);
+    let nuevoIndex = indexActual;
+    if(direccion === 'derecha') {
+      nuevoIndex++;
+    } else {
+      nuevoIndex--;
+    }
+    seleccionarProductoConIndex(nuevoIndex);
+  });
+}
 // seleccionamos producto por default
 seleccionarProductoConIndex(indexActual);
 
